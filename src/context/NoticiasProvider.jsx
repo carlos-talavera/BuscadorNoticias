6ce3dd80d0a902d0d7a9ac36deb3ccf78a5b4530
carlos-talavera <carloseduardorivera12@gmail.com>
2023-07-27 18:40:17 -0600

    first commit

---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect, createContext } from "react"
+import axios from "axios"
+
+const NoticiasContext = createContext();
+
+const NoticiasProvider = ({children}) => {
+
+    const [categoria, setCategoria] = useState('general');
+    const [noticias, setNoticias] = useState([]);
+    const [pagina, setPagina] = useState(1);
+    const [totalNoticias, setTotalNoticias] = useState(0);
+
+    // Cambio de categoría
+    useEffect(() => {
+
+        const consultarAPI = async () => {
+
+            const url = `${import.meta.env.VITE_BACKEND_URL}/api/${categoria}/1`;
+
+            const { data } = await axios(url);
+            setNoticias(data.articles);
+            setTotalNoticias(data.totalResults);
+            setPagina(1);
+
+        }
+
+        consultarAPI();
+
+    }, [categoria]);
+
+    // Cambio de página
+    useEffect(() => {
+
+        const consultarAPI = async () => {
+
+            const url = `${import.meta.env.VITE_BACKEND_URL}/api/${categoria}/${pagina}`;
+
+            const { data } = await axios(url);
+            setNoticias(data.articles);
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+
+        }
+
+        consultarAPI();
+
+    }, [pagina]);
+
+    const handleChangeCategoria = e => {
+
+        setCategoria(e.target.value);
+
+    }
+
+    const handleChangePagina = (e, valor) => {
+
+        setPagina(valor);
+
+    }
+
+    return(
+        <NoticiasContext.Provider
+            value={{
+                categoria,
+                handleChangeCategoria,
+                noticias,
+                totalNoticias,
+                handleChangePagina,
+                pagina
+            }}
+        >
+            {children}
+        </NoticiasContext.Provider>
+    )
+
+}
+
+export {
+    NoticiasProvider
+}
+
+export default NoticiasContext
